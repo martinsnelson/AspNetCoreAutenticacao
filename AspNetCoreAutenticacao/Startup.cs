b236e90f565d45c2f6acaf5e9fd1c4d92806e67b
Nelson de Figueiredo Martins <nelsontecti@gmail.com>
2019-03-01 22:03:35 -0300

    Adicionado autenticação via cookies

--- AspNetCoreAutenticacao/Startup.cs
+++ AspNetCoreAutenticacao/Startup.cs
@@ -38,7 +38,40 @@ namespace AspNetCoreAutenticacao
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddDefaultIdentity<IdentityUser>()
+                //.AddDefaultUI(UIFramework.Bootstrap4)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            services.Configure<IdentityOptions>(options =>
+             {
+                 // Password settings.
+                 options.Password.RequireDigit = false;
+                 options.Password.RequireLowercase = false;
+                 options.Password.RequireNonAlphanumeric = false;
+                 options.Password.RequireUppercase = false;
+                 options.Password.RequiredLength = 6;
+                 options.Password.RequiredUniqueChars = 1;
+
+                 // Lockout settings.
+                 options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+                 options.Lockout.MaxFailedAccessAttempts = 5;
+                 options.Lockout.AllowedForNewUsers = true;
+
+                 // User settings.
+                 options.User.AllowedUserNameCharacters =
+                 "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+                 options.User.RequireUniqueEmail = false;
+             });
+
+            services.ConfigureApplicationCookie(options =>
+            {
+                // Cookie settings
+                options.Cookie.HttpOnly = true;
+                options.ExpireTimeSpan = TimeSpan.FromMinutes(5);
+
+                options.LoginPath = "/Identity/Conta/Login";
+                options.AccessDeniedPath = "/Identity/Conta/AccessDenied";
+                options.SlidingExpiration = true;
+            });
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
